073c546c787d08e8471779f74d504605aeda2589
unknown <pwhynotfilms@gmail.com>
2019-07-12 23:23:34 -0500

    card shuffle working but not score tracking yet

--- my-app/src/components/Card/card.js
+++ my-app/src/components/Card/card.js
@@ -5,9 +5,11 @@ function Card(props) {
   return (
     <div className="card">
       <div className="img-container">
+      
+      <img alt={props.name} src={props.image} 
       value= {props.id} 
       onClick= {() => props.handleClick(props.id)}
-      <img alt={props.name} src={props.image} />
+      />
       </div>
       <div className="content">
         <ul>
